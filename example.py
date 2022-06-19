d0b4a2720c780fbde16e3efe0902e61e5d9598f0
Dominik Vach <vach.dominik@gmail.com>
2022-06-19 17:43:36 +0200

    simple example

---
+++ example.py
@@ -0,0 +1,16 @@
+from jira_easy_api_core import JiraClient
+
+
+jira_api_token="2pU1YDiGagbISzfGgDzfzE5C"
+jira_workspace_url='https://your_workspace.atlassian.net'
+
+
+jc=JiraClient(jira_workspace_url,"your_email@gmail.com",jira_api_token) #Initialize Jira Client
+
+issues=jc.get_all_issues_by_project_name("FP") #Get all issues/tasks
+jc.create_new_issue("FP", "Test task", "It works!") #Create new issue/task
+jc.update_issue_by_name("FP-377",{"description":"Test"}) #Update existing issue/task
+
+
+issues=jc.get_all_issues_by_project_name("FP")
+issue_ids=[str(issue) for issue in issues]
